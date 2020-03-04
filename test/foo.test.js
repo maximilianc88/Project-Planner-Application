771021895f742a49697e09c64ad7b66e65711ee2
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-03 21:12:04 -0800

    Maybe

--- test/foo.test.js
+++
@@ -1,9 +0,0 @@
-describe(`jest test`, () => {
-    it(`should pass`, () => {
-      expect(1 + 2).toEqual(3);
-    });
-  
-    it('should fail', () => {
-      expect('1' + '1').toEqual(2);
-    });
-  });
