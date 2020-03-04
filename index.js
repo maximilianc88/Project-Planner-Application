ea97784aa3510bac7ded62247ef0451ba10d6994
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-03 21:25:08 -0800

    Fingers crossed

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 "use strict";
+const accumulate = max => {
+  if (max === undefined || max === null || isNaN(max)) {
+    return null;
+  } else if (max < 1) {
+    return max;
+  } else {
+    let sum = 0;
+    for (let i = 1; i <= max; ++i) {
+      sum += i;
+    }
+    return sum;
+  }
+};
 
-const v = `My name`;
-console.log(v);
+module.exports = accumulate;
