7f5ae9970a1b411b15ffa401ec343992e4807c42
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-05 21:32:06 -0800

    Server.js edits for linter

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-`use strict`;
-
+"use strict";
 const express = require(`express`);
 const app = express();
 const PORT = process.env.PORT || 3000;
 const db = require(`./models`);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static(`public`));
-
 db.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () =>
     console.log(`App listening on http://localhost:${PORT}`)
