06b049c57ce43b3d7309933c9cba9a400ddcfdc5
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-07 01:16:48 -0800

    Team routes for get, post, put, delete

---
+++ routes/teams-routes.js
@@ -0,0 +1,47 @@
+"use strict";
+const db = require(`../models`);
+modules.exports = app => {
+  app.get(`/api/teams`, (req, res) => {
+    let teamName;
+    if (req.body.name) {
+      teamName = req.body.name;
+    }
+    db.Team.findAll({
+      where: { name: teamName },
+      include: [db.Task, db.Project, db.User]
+    }).then(dbTeam => {
+      res.json(dbTeam);
+    });
+  });
+  app.post(`/api/teams`, (req, res) => {
+    db.Team.create(req.body).then(dbTeam => {
+      res.json(dbTeam);
+    });
+  });
+  app.put(`/api/teams`, (req, res) => {
+    let teamName;
+    if (req.body.name) {
+      teamName = req.body.name;
+    }
+    db.Team.update(req.body, {
+      where: {
+        name: teamName
+      }
+    }).then(dbTeam => {
+      res.json(dbTeam);
+    });
+  });
+  app.delete(`/api/teams`, (req, res) => {
+    let teamName;
+    if (req.body.name) {
+      teamName = req.body.name;
+    }
+    db.Team.destroy({
+      where: {
+        name: teamName
+      }
+    }).then(dbTeam => {
+      res.json(dbTeam);
+    });
+  });
+};
