495e8e5b1475643f1d0845a99cdbb89c9a9b9f8a
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-03-07 12:22:42 -0800

    cleaned up sql seed. removed automatic fields

--- seeds.sql
+++ seeds.sql
@@ -1,34 +1,34 @@
 -- seeds for teams table
 INSERT INTO teams
-    (name, create_date, update_date)
+    (name)
 VALUES
-    ('ACES Team', '2017-01-01 01:00:00','2020-03-07 04:30:00'),
-    ('Dream Team', '1999-04-01 11:30:00','2010-11-15 12:20:00');
+    ('ACES Team'),
+    ('Dream Team');
 
 -- seeds for user table
 INSERT INTO users
-    (user_name, first_name, last_name, create_date, update_date, teamIdId)
+    (user_name, first_name, last_name, teamIdId)
 VALUES
-    ('Mike', 'Michael', 'Jordan', '2017-01-01 01:00:00','2020-03-07 04:30:00', 1),
-    ('Carrie', 'Carol', 'Nebraska', '1999-04-01 11:45:00','1999-04-01 11:45:00', 2);
+    ('Mike', 'Michael', 'Jordan', 1),
+    ('Carrie', 'Carol', 'Nebraska', 2);
 
 -- seeds for projects table
 INSERT INTO projects
-    (name, description, due_date, create_date, update_date, teamIdId)
+    (name, description, due_date, teamIdId)
 VALUES
-    ('Virtual reality', 'Fun stuff', '2020-03-14 09:52:42','2020-03-01 01:00:00','2020-03-07 05:00:00', NULL),
-    ('Chart.JS integration', 'incorporate chart.js features into project planner', '2020-03-14 06:30:00','2020-03-02 06:30:00','2020-03-07 11:27:00', 2),
-    ('Front-end bugs', 'Boring stuff', '2020-04-01 10:59:42','2019-10-25 07:33:42','2019-12-05 20:33:42', 1);
+    ('Virtual reality', 'Fun stuff', '2020-03-14 09:52:42', NULL),
+    ('Chart.JS integration', 'incorporate chart.js features into project planner', '2020-03-14 06:30:00', 2),
+    ('Front-end bugs', 'Boring stuff', '2020-04-01 10:59:42', 1);
 
 
 -- seeds for tasks table
 INSERT INTO tasks
-    (title, description, create_date, update_date, assigneeIdUserId)
+    (title, description, assigneeIdUserId)
 VALUES
-    ('Design VR hardware', 'Create device', '2020-01-11 08:07:42','2020-02-22 01:00:00', 1),
-    ('Design VR software', 'Create software', '2020-01-11 08:07:42','2020-03-05 18:00:00', 1),
-    ('Front-end bugs', 'Triage bugs from support teams', '1999-05-01 09:45:00','2020-03-01 12:00:00', 2),
-    ('Increase productivity', 'Automation', '2019-11-15 7:22:00','2019-11-15 7:22:00', null);
+    ('Design VR hardware', 'Create device', 1),
+    ('Design VR software', 'Create software', 1),
+    ('Front-end bugs', 'Triage bugs from support teams', 2),
+    ('Increase productivity', 'Automation', null);
 
 -- seeds for status table
     INSERT INTO status
