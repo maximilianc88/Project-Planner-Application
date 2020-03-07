6d75ea23d6581c73e8e456e64c9baa50f4209371
Richard Wang <richardchacha@hotmail.com>
2020-03-06 22:49:34 -0800

    change db config to dev, add express-handlebars module, add express-handlebars to server.js

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the database if it exists currently --
-DROP DATABASE IF EXISTS mothra;
+DROP DATABASE IF EXISTS mothra_development;
 -- Creates the database --
-CREATE DATABASE mothra;
+CREATE DATABASE mothra_development;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const db = require(`./models`);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(`public`));
+const expressHandlebars = require(`express-handlebars`);
+app.engine(`handlebars`, expressHandlebars({ defaultLayout: `main` }));
+app.set(`view engine`, `handlebars`);
 db.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () =>
     console.log(`App listening on http://localhost:${PORT}`)
