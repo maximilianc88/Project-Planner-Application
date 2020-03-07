2f40548ea40da4ce98fb041f6b6de92f9740c0df
Richard Wang <richardchacha@hotmail.com>
2020-03-07 12:46:24 -0800

    add navigation routes to render pages

--- routes/dev-routes.js
+++ routes/dev-routes.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const db = require(`../models`);
-
 module.exports = app => {
-  app.get(`/`, (req, res) => {
-    db.Project.findAll().then(result => {
-      res.render(`dev`, result);
-    });
-  });
+  app.get(`/`, (req, res) => res.render(`dashboard`));
+  app.get(`/dev`, (req, res) => res.render(`dev`));
+  app.get(`/projects`, (req, res) => res.render(`project`));
+  app.get(`/tasks`, (req, res) => res.render(`task`));
 };
