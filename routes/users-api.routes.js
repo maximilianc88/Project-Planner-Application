0e281eb196b232c15d5bcbdbd89ea4ac5267dc32
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-07 11:54:46 -0800

    Adjusted user get by id to render dashboard handlebars

--- routes/users-api.routes.js
+++ routes/users-api.routes.js
@@ -20,7 +20,7 @@ module.exports = app => {
       },
       include: [db.Task, db.Project, db.Team]
     }).then(dbUser => {
-      res.json(dbUser);
+      res.render(`dashboard`, dbUser);
     });
   });
   app.post(`/api/users`, (req, res) => {
