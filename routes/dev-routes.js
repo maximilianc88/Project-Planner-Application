d7572bf53de6a56b167d517467712070948e617b
Richard Wang <richardchacha@hotmail.com>
2020-03-07 14:55:59 -0800

    connect dashboard.js to dashboard.handlebars

--- routes/dev-routes.js
+++ routes/dev-routes.js
@@ -9,6 +9,11 @@ module.exports = app => {
   app.get(`/public/js/main.js`, (req, res) =>
     res.sendFile(path.join(__dirname, `../public/js/main.js`))
   );
+  app.get(`/public/js/dashboard.js`, (req, res) => {
+    const url = path.join(__dirname, `../public/js/dashboard.js`);
+    console.log(url);
+    res.sendFile(path.join(__dirname, `../public/js/dashboard.js`));
+  });
   app.get(`/`, (req, res) => res.render(`dashboard`));
   app.get(`/dev`, (req, res) => res.render(`dev`));
   app.get(`/projects`, (req, res) => res.render(`project`));
