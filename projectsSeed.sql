eeecfcdf536ebea7695567261c5fac69715d1e1c
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-03-07 12:02:03 -0800

    updated seeds for each table in the same file. Renamed seeds.sql file

--- projectsSeed.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO projects
-    (name, description, due_date, create_date, update_date, teamIdId)
-VALUES
-    ('John', 'Fun stuff', '2020-03-14 09:52:42','2020-03-01 01:00:00','2020-03-07 05:00:00', NULL);
