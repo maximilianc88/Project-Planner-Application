9f3806a835d605cd1931a51363d551db4d5befe8
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-06 17:34:44 -0800

    Fixed linter errors in index.js file as well as removed package.json and package-lock.json and models file from eslintignore

--- models/projects.js
+++ models/projects.js
@@ -1,39 +1,39 @@
-'use strict';
+"use strict";
 
-module.exports = function (sequelize, DataTypes) {
-    const Project = sequelize.define(`Project`, {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: true
-        },
-        due_date: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        team_id: {
-            type: DataTypes.INTEGER,
-            allowNull: true
-        }
+module.exports = function(sequelize, DataTypes) {
+  const Project = sequelize.define(`Project`, {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    due_date: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    team_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  });
+
+  Project.associate = function(models) {
+    // Associating Project with Tasks
+    // When a Project is deleted, also delete any associated Tasks
+    Project.hasMany(models.Task, {
+      foreignKey: `project_id`,
+      onDelete: `cascade`
     });
+  };
 
-    Project.associate = function (models) {
-        // Associating Project with Tasks
-        // When a Project is deleted, also delete any associated Tasks
-        Project.hasMany(models.Task, {
-            foreignKey: `project_id`,
-            onDelete: `cascade`
-        });
-    };
-
-    return Project;
+  return Project;
 };
