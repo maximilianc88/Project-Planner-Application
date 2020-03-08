1703ef58e4833b304921238e1ecf8fd80e36c5f5
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-03-08 15:03:54 -0700

    tasks now rendering on dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,22 +1,22 @@
 "use strict";
 $(document).ready(() => {
-  // const tasksContainer = $(`#tasks-container`);
   const projectsList = $(`#projects-list`);
-  // const tasksList = $(`#tasks-list`);
+  const tasksList = $(`#tasks-list`);
 
   let projects;
-  // let tasks;
+  let tasks;
 
-  const url = window.location.search;
-  let userId;
-  if (url.indexOf(`?user_id=`) !== -1) {
-    authorId = url.split(`=`)[1];
-    getPosts(userId);
-  }
+  // const url = window.location.search;
+  // let userId;
+  // if (url.indexOf(`?user_id=`) !== -1) {
+  //   authorId = url.split(`=`)[1];
+  //   getProjects(userId);
+  // }
   // If there's no userId we just get all posts as usual
-  else {
-    getProjects();
-  }
+  // else {
+  getProjects();
+  getTasks();
+  // }
 
   function getProjects() {
     // userId = user || ``;
@@ -41,8 +41,6 @@ $(document).ready(() => {
     }
     projectsList.append(projectsToAdd);
   }
-  // getProjects();
-  // getTasks();
 
   function createNewProjectsRow(project) {
     const newLi = $(`<li>${project.name}</li>`);
@@ -50,34 +48,29 @@ $(document).ready(() => {
     return newLi;
   }
 
-  // function createTasksRow(taskData) {
-  //   const newLi = $(`<li>`);
-  //   newLi.data(`task`, taskData);
-  //   newLi.append(`<li>${taskData.task}<li>`);
-  //   return newLi;
-  // }
+  function getTasks() {
+    $.get(`/api/tasks`, data => {
+      console.log(`Tasks`, data);
+      tasks = data;
+      if (!tasks || !tasks.length) {
+        $(`#tasks-list`.append(`<p>You have no tasks</p>`));
+      } else {
+        initializeTasksRows();
+      }
+    });
+  }
+  function initializeTasksRows() {
+    // projectsList.empty();
+    const tasksToAdd = [];
+    for (let i = 0; i < tasks.length; ++i) {
+      tasksToAdd.push(createNewTasksRow(tasks[i]));
+    }
+    tasksList.append(tasksToAdd);
+  }
 
-  // function getTasks() {
-  //   $.get(`/api/tasks`, data => {
-  //     for (let i = 0; i < data.length; ++i) {
-  //       rowsToAdd.push(createTasksRow(data[i]));
-  //     }
-  //     renderTasksList(rowsToAdd);
-  //     data.title.val(``);
-  //   });
-  // }
-
-  // function renderTasksList(rows) {
-  //   tasksList
-  //     .children()
-  //     .not(`:last`)
-  //     .remove();
-  //   tasksContainer.children(`.alert`).remove();
-  //   if (rows.length) {
-  //     console.log(rows);
-  //     tasksList.prepend(rows);
-  //   } else {
-  //     renderEmpty();
-  //   }
-  // }
+  function createNewTasksRow(task) {
+    const newTaskLi = $(`<li>${task.title}<li>`);
+    newTaskLi.data(`task`, task);
+    return newTaskLi;
+  }
 });
