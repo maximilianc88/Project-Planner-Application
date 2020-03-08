3b5d67cfaffbe851f4b6697a66c3710d716c41b5
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-03-08 00:23:09 -0800

    creating project list with html elements

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,39 +5,76 @@ $(document).ready(() => {
   const projectsList = $(`#projects-list`);
   const tasksList = $(`#tasks-list`);
 
-  getProjects();
-  getTasks();
+  let projects;
+  let tasks;
 
-  function createProjectsRow(projectData) {
-    const newLi = $(`<li>`);
-    newLi.data(`project`, projectData);
-    newLi.append(`<li>${projectData.project}<li>`);
+  const url = window.location.search;
+  let userId;
+  if (url.indexOf(`?user_id=`) !== -1) {
+    authorId = url.split(`=`)[1];
+    getPosts(userId);
+  }
+  // If there's no userId we just get all posts as usual
+  else {
+    getProjects();
+  }
+
+  function getProjects(user) {
+    userId = user || ``;
+    if (userId) {
+      userId = `/?user_id=${userId}`;
+    }
+    $.get(`/api/projects/${userId}`, data => {
+      console.log(`Projects`, data);
+      projects = data;
+      if (!projects || !projects.length) {
+        displayEmpty(user);
+      } else {
+        initializeRows();
+      }
+    });
+  }
+  // InitializeRows handles appending all of our constructed post HTML inside blogContainer
+  function initializeRows() {
+    projectsContainer.empty();
+    const projectsToAdd = [];
+    for (let i = 0; i < projects.length; ++i) {
+      projectsToAdd.push(createNewProjectRow(projects[i]));
+    }
+    projectsList.append(projectsToAdd);
+  }
+  // getProjects();
+  // getTasks();
+
+  function createNewProjectRow(project) {
+    const newLi = $(`<li>${project.name}</li>`);
+    newLi.data(`project`, project);
     return newLi;
   }
 
-  function getProjects() {
-    $.get(`/api/projects`, data => {
-      for (let i = 0; i < data.length; ++i) {
-        rowsToAdd.push(createProjectsRow(data[i]));
-      }
-      renderProjectsList(rowsToAdd);
-      nameInput.val(``);
-    });
-  }
+  // function getProjects(user) {
+  //   $.get(`/api/projects`, data => {
+  //     for (let i = 0; i < data.length; ++i) {
+  //       rowsToAdd.push(createProjectsRow(data[i]));
+  //     }
+  //     renderProjectsList(rowsToAdd);
+  //     data.name.val(``);
+  //   });
+  // }
 
-  function renderProjectsList(rows) {
-    projectsList
-      .children()
-      .not(`:last`)
-      .remove();
-    projectsContainer.children(`.alert`).remove();
-    if (rows.length) {
-      console.log(rows);
-      projectsList.prepend(rows);
-    } else {
-      renderEmpty();
-    }
-  }
+  // function renderProjectsList(rows) {
+  //   projectsList
+  //     .children()
+  //     .not(`:last`)
+  //     .remove();
+  //   projectsContainer.children(`.alert`).remove();
+  //   if (rows.length) {
+  //     console.log(rows);
+  //     projectsList.prepend(rows);
+  //   } else {
+  //     renderEmpty();
+  //   }
+  // }
 
   function createTasksRow(taskData) {
     const newLi = $(`<li>`);
@@ -52,7 +89,7 @@ $(document).ready(() => {
         rowsToAdd.push(createTasksRow(data[i]));
       }
       renderTasksList(rowsToAdd);
-      nameInput.val(``);
+      data.title.val(``);
     });
   }
 
