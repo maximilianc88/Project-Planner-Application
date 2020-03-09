4a5b16c0a4f974ba647f5bdbd9c49574ac3260f0
Richard Wang <richardchacha@hotmail.com>
2020-03-08 21:22:45 -0700

    update script src to public

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 "use strict";
 // Dependencies
 const express = require(`express`);
+
 // Set up Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 // Require database for sync
 const db = require(`./models`);
 const seed = require(`./seed`);
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Set up static file directory
 app.use(express.static(`public`));
+
 // Set Handlebars
 const expressHandlebars = require(`express-handlebars`);
 app.engine(`handlebars`, expressHandlebars({ defaultLayout: `main` }));
 app.set(`view engine`, `handlebars`);
+
 // Config routes
 require(`./routes/dev-routes`)(app);
 require(`./routes/projects-routes`)(app);
@@ -25,8 +31,10 @@ require(`./routes/teams-routes`)(app);
 require(`./routes/users-api-routes`)(app);
 
 // Syncing sequelize models and then starting our Express app
-db.sequelize.sync({ force: true }).then(() => seed()).then(() => {
-  app.listen(PORT, () =>
-    console.log(`App listening on http://localhost:${PORT}`)
-  );
-});
+db.sequelize.sync({ force: true })
+  .then(() => seed())
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`App listening on http://localhost:${PORT}`)
+    );
+  });
