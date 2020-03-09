9e36e868ccfd1cf944df6b035c42b0eda36ff5dd
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-08 17:57:06 -0700

    Charts rendering on page

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,20 +1,36 @@
 "use strict";
 
 $(document).ready(() => {
-  const chartCanvas = document.getElementById(`myChart`).getContext(`2d`);
+  const chartCanvas = document
+    .getElementById(`myChartTeamProjects`)
+    .getContext(`2d`);
+  window.chartColors = {
+    red: "rgb(255, 99, 132)",
+    orange: "rgb(255, 159, 64)",
+    yellow: "rgb(255, 205, 86)",
+    green: "rgb(75, 192, 192)",
+    blue: "rgb(54, 162, 235)",
+    purple: "rgb(153, 102, 255)",
+    grey: "rgb(231,233,237)"
+  };
   const chart = new Chart(chartCanvas, {
     // The type of chart we want to create
-    type: `line`,
-
+    type: `doughnut`,
     // The data for our dataset
     data: {
-      labels: [`January`, `February`, `March`, `April`, `May`, `June`, `July`],
+      labels: [`Unassigned`, `Open`, `In-Progress`, `Completed`, `Closed`],
+
       datasets: [
         {
           label: `My First dataset`,
-          backgroundColor: `rgb(255, 99, 132)`,
-          borderColor: `rgb(255, 99, 132)`,
-          data: [0, 10, 5, 2, 20, 30, 45]
+          backgroundColor: [
+            window.chartColors.red,
+            window.chartColors.orange,
+            window.chartColors.yellow,
+            window.chartColors.green,
+            window.chartColors.grey
+          ],
+          data: [4, 10, 8, 5, 2]
         }
       ]
     },
