fa7d7686ea19d61c2e9a57c9521373bae7aa8f2f
Richard Wang <richardchacha@hotmail.com>
2020-03-09 20:58:00 -0700

    parse dataValue

--- routes/dev-routes.js
+++ routes/dev-routes.js
@@ -5,30 +5,25 @@ const db = require(`../models`);
 
 module.exports = app => {
   app.get(`/node_modules/bulma/css/bulma.css`, (req, res) =>
-    res.sendFile(path.join(__dirname, `../node_modules/bulma/css/bulma.css`))
+    res.sendFile(path.join(__dirname, `bulma/css/bulma.css`))
   );
 
   app.get(`/`, (req, res) => res.render(`home`));
   app.get(`/dashboard`, (req, res) => res.render(`dashboard`));
-  // app.get(`/projects`, (req, res) => res.render(`project`));
   app.get(`/newProject`, (req, res) => res.render(`newProject`));
   app.get(`/tasks`, (req, res) => res.render(`task`));
   app.get(`/newTask`, (req, res) => res.render(`newTask`));
   app.get(`/stats`, (req, res) => res.render(`stats`));
   app.get(`/project/:id`, (req, res) => {
     const projectId = req.params.id;
-    console.log(projectId);
     db.Project.findOne({
       where: {
         id: projectId
       }
     })
       .then(hbs => {
-        console.log(hbs);
-        res.render(`project`);
-        // res.json(hbs);
+        console.log(hbs.dataValues);
+        res.render(`project`, hbs.dataValues );
       });
-
-    // res.render(`project`);
   });
 };
