852cb568bbe0bd42a9fe708ed16fce5b0ba0f8c3
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-03-10 21:39:38 -0700

    put request is working

--- public/js/viewTask.js
+++ public/js/viewTask.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const onReady = () => {
+
+  $(`#statusUpdateSubmit`).on(`click`, () => {
+    const updateStatus = {
+      id: $(`.title`).data(`taskid`),
+      // eslint-disable-next-line camelcase
+      status_code: $(`.select-status option:selected `).data(`id`)
+    };
+    console.log(updateStatus);
+    $.ajax(`/api/tasks`, {
+      type: `PUT`,
+      data: updateStatus
+    }).then(() => {
+      console.log(`Success`);
+    });
+  });
+};
+
+$(document).ready(onReady);
+
