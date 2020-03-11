a2fc9a424d7d943fecf9ff4988587eaaa9b6f4b8
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-11 00:48:02 -0700

    Adjusted stats page

--- public/js/stats.js
+++ public/js/stats.js
@@ -25,11 +25,6 @@ function getData() {
         closed += 1;
       }
     }
-    console.log(open);
-    console.log(closed);
-    console.log(inProgress);
-    console.log(unassigned);
-    console.log(complete);
   }).then(() => {
     const chartCanvas = document
       .getElementById(`myChartTeamProjects`)
@@ -70,38 +65,3 @@ function getData() {
   });
 }
 getData();
-
-const chartCanvas2 = document.getElementById(`myChart`).getContext(`2d`);
-window.chartColors = {
-  red: `#FF312B`,
-  orange: `#FF8438`,
-  yellow: `#FBFF4F`,
-  green: `#4EFF51`,
-  blue: `#156CB3`
-};
-const chart2 = new Chart(chartCanvas2, {
-  // The type of chart we want to create
-  type: `doughnut`,
-  // The data for our dataset
-  data: {
-    labels: [`Unassigned`, `Open`, `In-Progress`, `Completed`, `Closed`],
-
-    datasets: [
-      {
-        label: `My First dataset`,
-        backgroundColor: [
-          window.chartColors.red,
-          window.chartColors.orange,
-          window.chartColors.yellow,
-          window.chartColors.green,
-          window.chartColors.blue
-        ],
-        data: [four, ten, eight, five, two]
-      }
-    ]
-  },
-
-  // Configuration options go here
-  options: {}
-});
-console.log(chart2);
