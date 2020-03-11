f0f7e374db53b2a8dd2448e18a00eaf054a9c3c8
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-10 21:29:01 -0700

    Chart data is reflective of actual data

--- public/js/stats.js
+++ public/js/stats.js
@@ -30,45 +30,45 @@ function getData() {
     console.log(inProgress);
     console.log(unassigned);
     console.log(complete);
+  }).then(() => {
+    const chartCanvas = document
+      .getElementById(`myChartTeamProjects`)
+      .getContext(`2d`);
+    window.chartColors = {
+      red: `#FF312B`,
+      orange: `#FF8438`,
+      yellow: `#FBFF4F`,
+      green: `#4EFF51`,
+      blue: `#156CB3`
+    };
+    const chart1 = new Chart(chartCanvas, {
+      // The type of chart we want to create
+      type: `doughnut`,
+      // The data for our dataset
+      data: {
+        labels: [`Unassigned`, `Open`, `In-Progress`, `Completed`, `Closed`],
+
+        datasets: [
+          {
+            label: `My First dataset`,
+            backgroundColor: [
+              window.chartColors.red,
+              window.chartColors.orange,
+              window.chartColors.yellow,
+              window.chartColors.green,
+              window.chartColors.blue
+            ],
+            data: [unassigned, open, inProgress, complete, closed]
+          }
+        ]
+      },
+
+      // Configuration options go here
+      options: {}
+    });
   });
 }
 getData();
-
-const chartCanvas = document
-  .getElementById(`myChartTeamProjects`)
-  .getContext(`2d`);
-window.chartColors = {
-  red: `#FF312B`,
-  orange: `#FF8438`,
-  yellow: `#FBFF4F`,
-  green: `#4EFF51`,
-  blue: `#156CB3`
-};
-const chart1 = new Chart(chartCanvas, {
-  // The type of chart we want to create
-  type: `doughnut`,
-  // The data for our dataset
-  data: {
-    labels: [`Unassigned`, `Open`, `In-Progress`, `Completed`, `Closed`],
-
-    datasets: [
-      {
-        label: `My First dataset`,
-        backgroundColor: [
-          window.chartColors.red,
-          window.chartColors.orange,
-          window.chartColors.yellow,
-          window.chartColors.green,
-          window.chartColors.blue
-        ],
-        data: [four, ten, eight, five, two]
-      }
-    ]
-  },
-
-  // Configuration options go here
-  options: {}
-});
 
 const chartCanvas2 = document.getElementById(`myChart`).getContext(`2d`);
 window.chartColors = {
