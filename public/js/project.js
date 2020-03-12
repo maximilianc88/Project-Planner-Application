0d2f6e6e97921fa71a63b8cfa1c32763e3698d51
Richard Wang <richardchacha@hotmail.com>
2020-03-11 20:42:44 -0700

    able to edit project desc

--- public/js/project.js
+++ public/js/project.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// this is project.js
+const descriptionEl = $(`#description`);
+
+const editElementText = event => {
+  const targetId = $(event.target).attr(`id`);
+  const targetValue = $(event.target).text().trim();
+  const targetDataAttr = $(event.target).data(); // this is an object
+  console.log(targetId);
+  console.log(targetValue);
+  console.log(targetDataAttr);
+  const textEl = `<textarea class="textarea is-medium" id="${targetId}" rows="2" type="text">`;
+  $(`#${targetId}`).replaceWith(textEl);
+  $(`#${targetId}`).val(targetValue);
+
+  $(document).click(secondEvent => {
+    if (!$(secondEvent.target).closest(`#${targetId}`).length) {
+      const newText = $(`#${targetId}`).val();
+      const newParagraphEl = $(`<p class="title is-5" id="${targetId}">`);
+      newParagraphEl.text(newText);
+      newParagraphEl.data(targetDataAttr);
+      $(`#${targetId}`).replaceWith(newParagraphEl);
+    }
+  });
+};
+
+// event listeners
+descriptionEl.dblclick(event => {
+  editElementText(event);
+});
