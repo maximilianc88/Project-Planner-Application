aefa63a675777bb6bb234ca90577500b9cf3df35
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-12 13:59:01 -0700

    Task title is now editable

--- public/js/viewTask.js
+++ public/js/viewTask.js
@@ -16,6 +16,35 @@ const onReady = () => {
       location.reload();
     });
   });
+  const taskTitleEl = $(`#taskTitle`);
+  const editTaskTitleText = event => {
+    const taskTargetId = $(event.target).attr(`id`);
+    const taskTargetValue = $(event.target)
+      .text()
+      .trim();
+    const targetData = $(event.target).data();
+    console.log(taskTargetId);
+    console.log(taskTargetValue);
+    console.log(targetData);
+    const textEl = `<textarea class="textarea is-medium" id="${taskTargetId}" rows="2" type="text">`;
+    $(`#${taskTargetId}`).replaceWith(textEl);
+    $(`#${taskTargetId}`).val(taskTargetValue);
+
+    $(document).click(secondEvent => {
+      if (!$(secondEvent.target).closest(`#${taskTargetId}`).length) {
+        const newText = $(`#${taskTargetId}`).val();
+        const newParagraphEl = $(
+          `<div class="title is-2" data-taskId="{{ this.task.dataValues.id }}" id="id="${taskTargetId}">`
+        );
+        newParagraphEl.text(newText);
+        newParagraphEl.data(taskTargetValue);
+        $(`#${taskTargetId}`).replaceWith(newParagraphEl);
+      }
+    });
+  };
+  taskTitleEl.dblclick(event => {
+    editTaskTitleText(event);
+  });
 };
 
 $(document).ready(onReady);
