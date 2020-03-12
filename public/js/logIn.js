c524b15606ec07b475cb9efab922556de3480b8c
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2020-03-12 16:12:06 -0700

    reworked code so that it's testable

--- public/js/logIn.js
+++ public/js/logIn.js
@@ -1,44 +1,51 @@
 "use strict";
 
-$(document).ready(() => {
-  const signInButt = $(`#sign-in`);
-  const username = $(`#username`);
-  const passwordInput = $(`#password`);
-  const createAccount = $(`#create-account`);
+function getUserVerifier(userFormData) {
+  return responseData => {
+    const responseUserName = responseData.user_name;
+    const responsePassword = responseData.password;
+    if (
+      responseUserName === userFormData.user_name &&
+      responsePassword === userFormData.password
+    ) {
+      console.log(`proceed`);
+      window.location.assign(`/dashboard/${userFormData.user_name}`);
+    } else {
+      console.log(`failure`);
+    }
+  };
+}
 
+function loginUser(userFormData) {
+  $.get(`/api/users/usercheck/${userFormData.user_name}`, getUserVerifier(userFormData));
+}
+
+const onReady = () => {
   // Add event listener on the Create Account button to redirect them to the Sign Up page
-  createAccount.on(`click`, event => {
+  $(`#create-account`).on(`click`, event => {
     event.preventDefault();
     window.location.replace(`/signup`);
   });
 
   // Wchen the form is submitted, we validate there's a username
-  signInButt.on(`click`, event => {
+  $(`#sign-in`).on(`click`, event => {
     event.preventDefault();
-    const userData = {
+    const userFormData = {
       // eslint-disable-next-line camelcase
-      user_name: username.val().trim(),
-      password: passwordInput.val().trim()
+      user_name: $(`#username`).val().trim(),
+      password: $(`#password`).val().trim()
     };
-    if (!userData.user_name) {
+    if (!userFormData.user_name) {
       return;
     }
 
     // If we have a user name we run the loginUser function and clear the form
-    loginUser(userData.user_name);
-    function loginUser(user) {
-      const userURL = `/api/users/usercheck/${user}`;
-      $.get(userURL, data => {
-        const userNameData = data.user_name;
-        const userPasswordData = data.password;
-        if (userNameData === userData.user_name && userPasswordData === userData.password){
-          console.log(`proceed`);
-          window.location.replace(`/dashboard/${userData.user_name}`);
-        } else {
-          console.log(`failure`);
-        }
-        return;
-      });
-    }
+    loginUser(userFormData);
   });
-});
+};
+
+$(document).ready(onReady);
+
+if (typeof exports !== `undefined`) {
+  exports.getUserVerifier = getUserVerifier;
+}
