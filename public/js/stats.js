2548e7f904afd0ced051f4843caa3eb33013a6d6
Kaitlyn <kaitlyn.carlson7@gmail.com>
2020-03-13 14:08:44 -0700

    Rendering user task data

--- public/js/stats.js
+++ public/js/stats.js
@@ -121,6 +121,7 @@ $.get(`/api/users/`, data => {
       .getContext(`2d`);
     window.chartColors = {
       red: `#FF312B`,
+      orange: `#FF8438`,
       yellow: `#FBFF4F`,
       green: `#4EFF51`,
       blue: `#156CB3`
@@ -130,18 +131,25 @@ $.get(`/api/users/`, data => {
       type: `doughnut`,
       // The data for our dataset
       data: {
-        labels: [`Open`, `In-Progress`, `Completed`, `Closed`],
+        labels: [`Unassigned`, `Open`, `In-Progress`, `Completed`, `Closed`],
 
         datasets: [
           {
             label: `My First dataset`,
             backgroundColor: [
               window.chartColors.red,
+              window.chartColors.orange,
               window.chartColors.yellow,
               window.chartColors.green,
               window.chartColors.blue
             ],
-            data: [userOpen, userInProgress, userComplete, userClosed]
+            data: [
+              userUnassigned,
+              userOpen,
+              userInProgress,
+              userComplete,
+              userClosed
+            ]
           }
         ]
       },
