f0ff3c0ddb9a3d66154a643b81fd689b9ac0607c
Richard Wang <richardchacha@hotmail.com>
2020-03-12 20:19:44 -0700

    confirm affected rows

--- public/js/project.js
+++ public/js/project.js
@@ -21,8 +21,8 @@ $(document).ready(() => {
     $.ajax(`/api/project`, {
       type: `PUT`,
       data: newDataObj
-    }).then(() => {
-      console.log(`Success`);
+    }).then(res => {
+      console.log(`Affected rows: ${res}`);
     });
   };
 
@@ -34,51 +34,7 @@ $(document).ready(() => {
     const id = paraEl.data(`project-id`);
     const description = textareaEl.val().trim();
     const newDataObj = { id, description};
-    console.log(newDataObj);
     putNewDescription(newDataObj);
-  };
-
-
-
-  const finishEditElement = textareaEl => {
-    $(document).click(event => {
-      if (!$(event.target).closest(textareaEl.length)) {
-        const newText = textareaEl.val();
-        console.log(newText);
-        const parentEl = textareaEl.parent();
-        parentEl.empty();
-        // const newParagraphEl = $(`<p>`);
-        // newParagraphEl.text(newText);
-        // newParagraphEl.data(targetId);
-        // parentEl.replaceWith(newParagraphEl);
-        // $(`#${targetId}`).replaceWith(newParagraphEl);
-      }
-    });
-  };
-
-  const editElementText = event => {
-    console.log($(event.target));
-    $(event.target).hide();
-    // if ($(event.target).attr(`hidden`) === `false`) {
-    //   console.log(`false`);
-    // }
-    const parentEl = $(event.target).parent();
-    const parentElementId = parentEl.attr(`id`); // this is the html element id
-    const targetValue = $(event.target).text().trim(); // this is description
-    const targetDataAttr = $(event.target).data(); // this is an object
-    console.log(parentElementId);
-    console.log(targetValue);
-    console.log(targetDataAttr);
-    const textareaEl = $(event.target).siblings(`textarea`);
-    console.log(textareaEl);
-    textareaEl.val(targetValue);
-    textareaEl.css(`display`, `block`);
-    // const textareaEl = $(`<textarea class="textarea is-medium" rows="2" type="text">`);
-    // textareaEl.val(targetValue);
-    // parentEl.append(textareaEl);
-    // $(`#${targetId}`).replaceWith(textEl);
-    // $(`#${targetId}`).val(targetValue);
-
   };
 
   // event listeners
